8e78bc32c30aab73bf3283fdcc86fa1f85da5c83
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-09-21 14:20:05 +0900

    feat: detail cause of error

--- src/mod.ts
+++ src/mod.ts
@@ -3,14 +3,19 @@ import { createFeed, fetchFeedError } from "./feed/mod.ts";
 export default {
   async fetch(req) {
     if (req.method !== "GET") {
-      return new Response("Method Not Allowed", { status: 405 });
+      return new Response("Method Not Allowed: Only GET method allowed.", {
+        status: 405,
+      });
     }
 
     const accepts = (req.headers.get("accept") ?? "*/*")
       .split(",")
       .map((s) => s.trim());
     if (!(accepts.includes("application/json") || accepts.includes("*/*"))) {
-      return new Response("Not Acceptable", { status: 406 });
+      return new Response(
+        "Unsupported Type: Only application/json content type supported.",
+        { status: 406 },
+      );
     }
 
     try {
